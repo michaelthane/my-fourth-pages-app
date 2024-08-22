36cc5388c74095ce3daa7d6354364211d79fe7d6
Michael Thane <mthane@cloudflare.com>
2024-08-22 13:51:07 -0500

    created a policy for my pages application

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -4,6 +4,6 @@ import { PagesFunction } from "@cloudflare/workers-types";
 export const onRequest: PagesFunction = cloudflareAccessPlugin({
   domain: "https://thanenotes.cloudflareaccess.com",
   // aud: "bc065b6b1e7bee66c8afebe8e8cc13b0e450fb9614bebe554e3f319528308f4a",
-  aud: "bc19729563fe5aa6a46bfab12b174e61e8e726d510e6c83953ebb83cbb235e3a",
+  aud: "c2df1929795cdddc558843fddf12eb52f043159c51a6379931dc3c43e090c73c",
   // aud: "b6159909a-abb6-456b-883d-7370609d82cf"
 });
