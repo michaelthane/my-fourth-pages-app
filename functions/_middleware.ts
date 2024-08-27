46cc80c38d40bfdb34d9b976ebe905daceeb08b8
Michael Thane <mthane@cloudflare.com>
2024-08-27 02:25:32 -0500

    token test in getToken.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,9 +1,18 @@
 import cloudflareAccessPlugin from "@cloudflare/pages-plugin-cloudflare-access";
 import { PagesFunction } from "@cloudflare/workers-types";
 
-export const onRequest: PagesFunction = cloudflareAccessPlugin({
-  domain: "https://thanenotes.cloudflareaccess.com",
-  // aud: "bc065b6b1e7bee66c8afebe8e8cc13b0e450fb9614bebe554e3f319528308f4a",
-  aud: "c2df1929795cdddc558843fddf12eb52f043159c51a6379931dc3c43e090c73c",
-  // aud: "b6159909a-abb6-456b-883d-7370609d82cf"
-});
+// export const onRequest: PagesFunction = cloudflareAccessPlugin({
+//   domain: "https://thanenotes.cloudflareaccess.com",
+//   aud: "c2df1929795cdddc558843fddf12eb52f043159c51a6379931dc3c43e090c73c", // This is the AUD tag for "my-fourth-pages-app - Cloudflare Pages" found in Zero Trust/Applications/Access
+// });
+
+export const onRequest: PagesFunction = (context: any) => {
+    if ('BYPASS_AUTH' in context.env && context.env.BYPASS_AUTH === 'true') {
+      return context.next();
+    }
+  
+    return cloudflareAccessPlugin({
+      domain: "https://thanenotes.cloudflareaccess.com",
+      aud: "c2df1929795cdddc558843fddf12eb52f043159c51a6379931dc3c43e090c73c",
+    })(context);
+  };
---
+++ functions/getToken.ts
@@ -0,0 +1,10 @@
+import type { PluginData } from "@cloudflare/pages-plugin-cloudflare-access";
+
+export const onRequest: PagesFunction<unknown, any, PluginData> = async ({
+  data,
+}) => {
+  console.log('data', data);
+  return new Response(
+    `Hello, ${data.cloudflareAccess.JWT.payload.email || "service user"}!`,
+  );
+};
